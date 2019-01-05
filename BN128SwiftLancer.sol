6b642cc7fd3bff6dfd7eb76c90c35bfc36680fcf
Maxilbert <Maxilbert@users.noreply.github.com>
2019-01-05 18:37:41 -0500

    Update BN128SwiftLancer.sol

--- BN128SwiftLancer.sol
+++ BN128SwiftLancer.sol
@@ -14,8 +14,6 @@ contract SwiftLancer{
         
     using BN128 for *;
 
-    event debug(bytes32 a);
-    
     event FillGold(string a);
     
     event OpenGold(string b);
@@ -29,12 +27,11 @@ contract SwiftLancer{
         uint c2_y
     );
     
-    event Answers_Test(string b);
+    SwiftLancerParameters parameters;
     
     // answers of workers
     struct answers {
         bytes32[106] ciphers;
-        uint counter;
         uint[3] err_indexes;
         uint err_counter;
     }
@@ -42,8 +39,6 @@ contract SwiftLancer{
     address public requester;
     uint[6] public actual_gold_indices;
     uint[6] public actual_gold_solutions;
-    
-    SwiftLancerParameters parameters;
     
 
     bytes32 public task_swarm_addr = 0xb833be6d483c981488a6b0f32fd133f9f7b8810a9663becb38359c1731210529;
@@ -78,7 +73,7 @@ contract SwiftLancer{
     // opening the commitments and checking if it matches with the committed values
     function opening_phase(uint[] indices, uint[] sols, uint r_val) returns (bool){
         //require(msg.sender == requester);
-        for(uint i=0;i<6;i++){
+        for (uint i=0;i<6;i++) {
             bytes32 lhs1 = sha3(sha3(indices[i]), sha3(r_val));
             bytes32 lhs2 = sha3(sha3(sols[i]), sha3(r_val));
             
@@ -99,7 +94,7 @@ contract SwiftLancer{
     
     // different plaintext proof for proving that worker did not provide correct solution of golden standard questions
     function different_plaintext_proof(uint c1_x, uint c1_y, uint c2_x, uint c2_y, uint q_index, address worker, uint a_x, uint a_y, uint z) returns (bool){
-        for(uint i=0; i<6; i++){
+        for (uint i=0; i<6; i++) {
             if(q_index == actual_gold_indices[i]){
                 if (answers_map[worker].err_indexes[0] == q_index) {
                     return false;
@@ -110,20 +105,16 @@ contract SwiftLancer{
                 if (answers_map[worker].err_indexes[2] == q_index) {
                     return false;
                 }
-                if(answers_map[worker].ciphers[q_index] == sha3(c1_x, c1_y, c2_x, c2_y)){
-                    //emit PlainTextProof("Hashes of Ciphertexts match");
-                    if (check_proof(c1_x, c1_y, c2_x, c2_y, a_x, a_y, z, 1 - actual_gold_solutions[i])) {
+                if(answers_map[worker].ciphers[q_index] == sha3(c1_x, c1_y, c2_x, c2_y)) {
+                    if (check_proof(c1_x, c1_y, c2_x, c2_y, a_x, a_y, z, 1-actual_gold_solutions[i])) {
                         answers_map[worker].err_indexes[answers_map[worker].err_counter] = q_index;
                         answers_map[worker].err_counter += 1;
                         emit PlainTextProof("Different Plaintext Verified!");
                         return true;
-                    }
-                    else {
+                    } else {
                         return false;
                     }
-                }
-                else{
-                    //emit PlainTextProof("Hashes of Ciphertexts don't match");
+                } else {
                     return false;
                 }
             }
@@ -133,13 +124,14 @@ contract SwiftLancer{
     
     
     //
-    function check_proof(uint c1_x, uint c1_y, uint c2_x, uint c2_y, uint a_x, uint a_y, uint z, uint solexp) internal returns(bool) {
+    function check_proof(uint c1_x, uint c1_y, uint c2_x, uint c2_y, uint a_x, uint a_y, uint z, uint m) internal returns(bool) {
         uint c = prepare_nizk_challenge(a_x, a_y);
         BN128.G1Point memory rhs = different_plaintext_proof_rhs(c2_x, c2_y, a_x, a_y, c);
         BN128.G1Point memory lhs = different_plaintext_proof_lhs(c1_x, c1_y, z);
-        if(solexp == 1) {
-            lhs = BN128.add(BN128.mul(parameters.get_g(), c), lhs);
-            //lhs = lhs.modmul(parameters.get_g().prepare_modexp(c, p), p);
+        if(m == 1) {
+            BN128.G1Point memory M = BN128.mul(BN128.G1Point(1, 2), c);
+            BN128.G1Point memory lhs1 = BN128.add(M, lhs);
+            return (lhs1.X == rhs.X && lhs1.Y == rhs.Y);
         }
         return (lhs.X == rhs.X && lhs.Y == rhs.Y);
     }
@@ -147,7 +139,7 @@ contract SwiftLancer{
     
     // 
     function prepare_nizk_challenge(uint a_x, uint a_y) internal returns (uint) {
-        return (uint(sha3(a_x, a_y)) >> 128);
+        return (uint(sha3(a_x, a_y)) >> 130);
     }
     
     
@@ -167,59 +159,27 @@ contract SwiftLancer{
     
     
     // workers submitting answers to the contract
-    function submit_answers(uint[40] memory i, uint[40] memory c1_x, uint[40] memory c1_y, uint[40] memory c2_x, uint[40] memory c2_y) {
+    //function submit_answers(uint[106] memory i, uint[106] memory c1_x, uint[106] memory c1_y, uint[106] memory c2_x, uint[106] memory c2_y) {
+    function submit_answers(uint[106] memory c1_x, uint[106] memory c1_y, uint[106] memory c2_x, uint[106] memory c2_y) {
         address worker = msg.sender;
-        for (uint j = 0; j < 40; j++) {
-            bytes32 hash = sha3(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
+        bytes32 hash = sha3(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
+        emit Ciphertexts(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
+        answers hisAnswers;
+        hisAnswers.ciphers[0] = hash;
+        answers_map[worker] = hisAnswers;
+        for (uint j = 1; j < 106; j++) {
+            hash = sha3(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
             emit Ciphertexts(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
-            if (i[j] > 0) {
-                answers_map[worker].ciphers[i[j]] = hash;
-                answers_map[worker].counter += 1;
-            } else {
-                answers hisAnswers;
-                hisAnswers.ciphers[0] = hash;
-                answers_map[worker] = hisAnswers;
-                answers_map[worker].counter = 1;
-            }
-            if (answers_map[worker].counter == 106 && workers_counter < 4) {
-                workers[workers_counter] = worker;
-                workers_counter += 1;
-                answers_map[worker].err_indexes = [106,106,106];
-            }
+            answers_map[worker].ciphers[j] = hash;
+        }
+        if (workers_counter < 4) {
+            workers[workers_counter] = worker;
+            workers_counter += 1;
+            answers_map[worker].err_indexes = [106,106,106];
         }
     }
     
     
-    // workers submitting answers to the contract
-    function submit_answers(uint[33] memory i, uint[33] memory c1_x, uint[33] memory c1_y, uint[33] memory c2_x, uint[33] memory c2_y) {
-        address worker = msg.sender;
-        for (uint j = 0; j < 33; j++) {
-            bytes32 hash = sha3(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
-            emit Ciphertexts(c1_x[j],c1_y[j],c2_x[j],c2_y[j]);
-            if (i[j] > 0) {
-                answers_map[worker].ciphers[i[j]] = hash;
-                answers_map[worker].counter += 1;
-            } else {
-                answers hisAnswers;
-                hisAnswers.ciphers[0] = hash;
-                answers_map[worker] = hisAnswers;
-                answers_map[worker].counter = 1;
-            }
-            if (answers_map[worker].counter == 106 && workers_counter < 4) {
-                workers[workers_counter] = worker;
-                workers_counter += 1;
-                answers_map[worker].err_indexes = [106,106,106];
-            }
-        }
-    }
 
-    
-    function toBytes(uint256 x) returns (bytes memory b) {
-        b = new bytes(32);
-        assembly { mstore(add(b, 32), x) }
-    }
-    
-    function toBytes(bytes32 _data) public pure returns (bytes memory) {
-        return abi.encodePacked(_data);
-    }
+
 }
